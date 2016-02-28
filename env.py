5c034aa334600f8cb6de8ce361d1bb788446e279
Miles Rout <miles.rout@gmail.com>
2016-02-28 20:11:55 +1300

    Fix indentation and trailing newlines.

--- env.py
+++ env.py
@@ -1,19 +1,19 @@
 class Env:
-	def __init__(self, bindings=None):
-		if bindings is None:
-			self.bindings = {}
-		else:
-			self.bindings = bindings
-	def lookup(self, name):
-		return self.bindings[name]
+    def __init__(self, bindings=None):
+        if bindings is None:
+            self.bindings = {}
+        else:
+            self.bindings = bindings
+    def lookup(self, name):
+        return self.bindings[name]
 
-	def __add__(self, other):
-		new_bindings = {}
-		for k, v in other.bindings.items():
-			new_bindings[k] = v
-		for k, v in self.bindings.items():
-			new_bindings[k] = v
-		return Env(new_bindings)
+    def __add__(self, other):
+        new_bindings = {}
+        for k, v in other.bindings.items():
+            new_bindings[k] = v
+        for k, v in self.bindings.items():
+            new_bindings[k] = v
+        return Env(new_bindings)
 
-	def __radd__(self, other):
-		return self + other
+    def __radd__(self, other):
+        return self + other
