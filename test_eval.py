bc90dfe3086d5efc387606527ea596375ef548a0
Miles Rout <miles.rout@gmail.com>
2016-09-25 21:43:27 +1300

    Add a test for closure support

--- test_eval.py
+++ test_eval.py
@@ -32,6 +32,17 @@ class TestEval(unittest.TestCase):
             ), self.base_env),
             6)
 
+    def test_closure(self):
+        self.assertEqual(
+            visp.evaluate(visp.read(
+                """((lambda (x)
+                      ((lambda (y)
+                         (+ x y))
+                        (+ 1 2)))
+                     1)"""
+            ), self.base_env),
+            4)
+
     def test_quote(self):
         self.assertEqual(
             visp.evaluate(visp.read(
